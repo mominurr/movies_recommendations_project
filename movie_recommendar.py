419410def44f0e9d21682c4cd26812ac2287d98c
Mominur Rahman <mominurr518@gmail.com>
2023-09-14 02:44:51 +0600

    without csv file uploading python file

---
+++ movie_recommendar.py
@@ -0,0 +1,92 @@
+import pandas as pd
+from math import sqrt
+
+
+#Storing the movie information into a pandas dataframe
+movies_df = pd.read_csv(r'.\movies_recommendations_project\moviedataset\movies.csv')
+#Storing the user information into a pandas dataframe
+ratings_df = pd.read_csv(r'.\movies_recommendations_project\moviedataset\ratings.csv')
+#Head is a function that gets the first N rows of a dataframe. N's default is 5.
+
+
+#Using regular expressions to find a year stored between parentheses
+#We specify the parantheses so we don't conflict with movies that have years in their titles
+movies_df['year'] = movies_df['title'].str.extract('(\(\d\d\d\d\))',expand=False)
+#Removing the parentheses
+movies_df['year'] = movies_df['year'].str.extract('(\d\d\d\d)',expand=False)
+#Removing the years from the 'title' column
+# Remove the year from movie titles using a lambda function
+movies_df['title'] = movies_df['title'].apply(lambda x: x.split('(')[0].strip())
+#Applying the strip function to get rid of any ending whitespace characters that may have appeared
+# movies_df['title'] = movies_df['title'].apply(lambda x: x.strip())
+
+#Every genre is separated by a | so we simply have to call the split function on |
+movies_df['genres'] = movies_df['genres'].str.split('|')
+
+
+
+
+#Copying the movie dataframe into a new one since we won't need to use the genre information in our first case.
+moviesWithGenres_df = movies_df.copy()
+
+#For every row in the dataframe, iterate through the list of genres and place a 1 into the corresponding column
+for index, row in movies_df.iterrows():
+    for genre in row['genres']:
+        moviesWithGenres_df.at[index, genre] = 1
+#Filling in the NaN values with 0 to show that a movie doesn't have that column's genre
+moviesWithGenres_df = moviesWithGenres_df.fillna(0)
+
+
+#Drop removes a specified row or column from a dataframe
+ratings_df = ratings_df.drop('timestamp',axis='columns')
+
+
+
+userInput = [
+            {'title':'Breakfast Club, The', 'rating':5},
+            {'title':'Toy Story', 'rating':3.5},
+            {'title':'Jumanji', 'rating':2},
+            {'title':"Pulp Fiction", 'rating':5},
+            {'title':'Akira', 'rating':4.5}
+         ] 
+inputMovies = pd.DataFrame(userInput)
+
+
+#Filtering out the movies by title
+inputId = movies_df[movies_df['title'].isin(inputMovies['title'].tolist())]
+
+#Then merging it so we can get the movieId. It's implicitly merging it by title.
+inputMovies = pd.merge(inputId, inputMovies)
+#Dropping information we won't use from the input dataframe
+inputMovies = inputMovies.drop('genres',axis='columns').drop('year',axis='columns')
+#Final input dataframe
+#If a movie you added in above isn't here, then it might not be in the original 
+#dataframe or it might spelled differently, please check capitalisation.
+
+
+#Filtering out the movies from the input
+userMovies = moviesWithGenres_df[moviesWithGenres_df['movieId'].isin(inputMovies['movieId'].tolist())]
+#Resetting the index to avoid future issues
+userMovies = userMovies.reset_index(drop=True)
+#Dropping unnecessary issues due to save memory and to avoid issues
+userGenreTable = userMovies.drop('movieId', axis='columns').drop('title', axis='columns').drop('genres', axis='columns').drop('year', axis='columns')
+
+#Dot produt to get weights
+userProfile = userGenreTable.transpose().dot(inputMovies['rating'])
+#The user profile
+
+#Now let's get the genres of every movie in our original dataframe
+genreTable = moviesWithGenres_df.set_index(moviesWithGenres_df['movieId'])
+#And drop the unnecessary information
+genreTable = genreTable.drop('movieId', axis='columns').drop('title', axis='columns').drop('genres', axis='columns').drop('year', axis='columns')
+
+#Multiply the genres by the weights and then take the weighted average
+recommendationTable_df = ((genreTable*userProfile).sum(axis=1))/(userProfile.sum())
+
+#Sort our recommendations in descending order
+recommendationTable_df = recommendationTable_df.sort_values(ascending=False)
+
+
+#The final recommendation table
+recomandations=movies_df.loc[movies_df['movieId'].isin(recommendationTable_df.head(20).keys())]
+print(recomandations)
